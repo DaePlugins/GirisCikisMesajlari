4e742bc6b080a6663ea39a683d61d97ef4170445
Daemonn <callahan_555@hotmail.com>
2019-08-28 05:16:21 +0300

    Birkaç ayarın sırası değiştirildi.

--- GirişÇıkışMesajlarıYapılandırma.cs
+++ GirişÇıkışMesajlarıYapılandırma.cs
@@ -8,18 +8,17 @@ namespace DaeGirisCikisMesajlari
     {
         public bool OyuncuSayısıAktif { get; set; }
         public ushort OyuncuSayısıEfektIdsi { get; set; }
-
-        public bool GirişMesajlarıAktif { get; set; }
-        public string GirişMesajıRengi { get; set; }
-        public ushort GirişEfektiIdsi { get; set; }
-
-        public bool ÇıkışMesajlarıAktif { get; set; }
-        public string ÇıkışMesajıRengi { get; set; }
-        public ushort ÇıkışEfektiIdsi { get; set; }
-
         public string MevcutOyuncuSayısıRengi { get; set; }
         public string MaksimumOyuncuSayısıRengi { get; set; }
-        
+
+        public bool GirişMesajlarıAktif { get; set; }
+        public ushort GirişEfektiIdsi { get; set; }
+        public string GirişMesajıRengi { get; set; }
+
+        public bool ÇıkışMesajlarıAktif { get; set; }
+        public ushort ÇıkışEfektiIdsi { get; set; }
+        public string ÇıkışMesajıRengi { get; set; }
+
         public bool ÖzelEfektlerAktif { get; set; }
         [XmlArrayItem("EfektIdsi")]
         public List<ushort> EfektIdleri { get; set; } = new List<ushort>();
@@ -28,17 +27,16 @@ namespace DaeGirisCikisMesajlari
         {
             OyuncuSayısıAktif = true;
             OyuncuSayısıEfektIdsi = 15964;
-
-            GirişMesajlarıAktif = true;
-            GirişMesajıRengi = "32FF32";
-            GirişEfektiIdsi = 15963;
-
-            ÇıkışMesajlarıAktif = true;
-            ÇıkışMesajıRengi = "FF3232";
-            ÇıkışEfektiIdsi = 15963;
-
             MevcutOyuncuSayısıRengi = "FFC800";
             MaksimumOyuncuSayısıRengi = "64FF00";
+
+            GirişMesajlarıAktif = true;
+            GirişEfektiIdsi = 15963;
+            GirişMesajıRengi = "32FF32";
+
+            ÇıkışMesajlarıAktif = true;
+            ÇıkışEfektiIdsi = 15963;
+            ÇıkışMesajıRengi = "FF3232";
 
             ÖzelEfektlerAktif = false;
             EfektIdleri = new List<ushort>
